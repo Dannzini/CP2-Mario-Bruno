d99e2643c9ce997f2685e210192289c644e1306f
Dannzini <bruno.bbs.09@gmail.com>
2024-04-12 20:52:50 -0300

    Alterando o Migration

---
+++ ApiUsuario/Migrations/20240412234555_Usuario.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiUsuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Senha = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CPF = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Cargo = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Administrador = table.Column<bool>(type: "NUMBER(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
